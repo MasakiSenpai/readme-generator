5bcbb2fc297982d9f06838e5229f9af39f514aeb
MasakiDesktop <mandij50@gmail.com>
2023-01-27 16:16:34 -0500

    skeleton functions and started questions

---
+++ index.js
@@ -0,0 +1,29 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const prompts = []
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'Please ',
+            message: ''
+        },
+        {
+
+        }
+    ])
+
+    function init() {
+
+    }
+
+    function LicenseBadges() {
+        
+    }
